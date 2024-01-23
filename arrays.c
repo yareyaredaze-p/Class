8f4dc57c99b790016a3966ba38ccf2d44f2f96e8
Prajwol Bohara <prajwolbohara@Prajwols-MacBook-Air.local>
2024-01-23 10:09:44 +0545

    calculator

--- arrays.c
+++ arrays.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int arr[5],sum=0;
+    int arr[5],sum=0,ecount=0,ocount=0;
     for(int i=0;i<5;i++){ 
     printf("Enter values to put in array :");
     scanf("%d",&arr[i]);
@@ -11,7 +11,18 @@ int main(){
     for(int i=0;i<5;i++){
         sum+=arr[i];
         }
-        printf("sum =%d",sum);
+        printf("sum =%d\n",sum);
+
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        if(arr[i]%2==0){
+           ecount++;
+        }
+        else{ocount++;}
+    }
+    printf("%d %d",ecount,ocount);
     return 0;
 
 
