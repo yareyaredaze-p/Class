c9a9f9923cef6e110953496b56abf4c537821dde
Prajwol Bohara <prajwolbohara@Prajwols-MacBook-Air.local>
2024-02-13 11:02:02 +0545

    Class

---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void largest_num(int,int);
+int main({
+    int number1,number2;
+    printf("enter the numbers :");
+    scanf("%d %d",&number1,&number2);
+    largest_num(number1,number2);
+}
